63253400fa36bf81dffa01581202102a73bcef84
GabriellePelletero <gcpelletero@up.edu.ph>
2024-03-21 14:23:44 +0800

    3rd commit

--- mongoose-basics.js
+++ mongoose-basics.js
@@ -30,22 +30,24 @@ const newStudent = new Student({
 await newStudent.save();
 
 // simple update
-let stud = await Student.findOne({ age: 65});
-stud.fname = "Peter"
+let stud = await Student.findOne({ age: 28});
+stud.fname = "Peter";
 await stud.save();
 
-// updates a single document and returns an object containing the result details (NOT the document).
-await Student.updateOne(
-    { age: 17 },
-    {$set: {fname: "John"}}
-  );
-  
-  // updates multiple documents and returns an object containing the result details (NOT the documents)
-  await Student.updateMany(
-    { age: 65 },
-    {$set: {age: 17}}
-  );
-  
-  await Student.deleteOne({ stdnum: '8051495845'})
+console.log(stud);
 
-await Student.deleteMany({ fname: "John"})
+// // updates a single document and returns an object containing the result details (NOT the document).
+// await Student.updateOne(
+//     { age: 17 },
+//     {$set: {fname: "John"}}
+//   );
+  
+//   // updates multiple documents and returns an object containing the result details (NOT the documents)
+//   await Student.updateMany(
+//     { age: 65 },
+//     {$set: {age: 17}}
+//   );
+  
+//   await Student.deleteOne({ stdnum: '8051495845'})
+
+// await Student.deleteMany({ fname: "John"})
